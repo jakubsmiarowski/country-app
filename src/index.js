278c4fc8ef2b79807a18e132b1eb223b9f1d80cf
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-09-03 21:32:23 +0200

    28.4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
 import store from './store';
-import DevTools from './DevTools';
+import { getCountries } from './actions/actions-countries';
+//import DevTools from './DevTools';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
-        <DevTools />
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
